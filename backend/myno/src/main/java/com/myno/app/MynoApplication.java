7cb45d9a7329282d865d052fe7ad09b044aaac7b
winrin <tjdnfls1234@naver.com>
2022-12-03 04:02:41 +0900

    init project

---
+++ backend/myno/src/main/java/com/myno/app/MynoApplication.java
@@ -0,0 +1,13 @@
+package com.myno.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynoApplication.class, args);
+	}
+
+}
---
+++ backend/myno/src/test/java/com/myno/app/MynoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myno.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
